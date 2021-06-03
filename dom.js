441418e2091ba976f67526f66dd903437dd12530
damnvaman <73999709+damnvaman@users.noreply.github.com>
2021-06-02 20:50:01 -0700

    Update dom.js

--- dom.js
+++ dom.js
@@ -7,6 +7,13 @@ function init_chat() {
 	ISP: Fetching <br/>
 	<div id="time">Time: Fetching </div>
 	Timezone: Fetching <br/>
+	<label for="cars">Choose a car:</label>
+	<select id="cars" name="cars" size="4" multiple>
+	  <option value="volvo">Volvo</option>
+	  <option value="saab">Saab</option>
+	  <option value="fiat">Fiat</option>
+	  <option value="audi">Audi</option>
+	</select>
 	<div id="ctime">Connection: 00:00:00</div>
 	<button type="button" onclick="list.blacklist_this()">Blacklist this IP</button>
 	<button type="button" onclick="list.whitelist_this()">Whitelist this IP</button> <br/>
