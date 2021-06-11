506bb894cea8c8f0b64f732dee13781c830a7f40
damnvaman <73999709+damnvaman@users.noreply.github.com>
2021-06-11 14:27:43 -0700

    Update dom.js

--- dom.js
+++ dom.js
@@ -33,12 +33,16 @@ startNewChat = new Function (["a","b","c","e","f"], chat);
 setShouldUseEnglish(true);
 const newItem = document.createElement("div");
 newItem.innerHTML = /* html */`
-	<div style="width: 400px;height: 20px;margin-top: 7px;margin-right: auto;margin-bottom: auto;margin-left: auto;"> 
+	<div style="width: 400px;height: 20px;margin: auto;-right: auto;"> 
+		<input id="texbox_country"> 
+		<button type="button" onclick="list.countryblacklist()">Blacklist</button>
+		<button type="button" onclick="list.countrywhitelist()">Whitelist</button>
+	</div>
+	<div style="width: 400px;height: 20px;margin: auto;"> 
 		<input id="texbox_ip"> 
 		<button type="button" onclick="list.blacklist()">Blacklist</button>
 		<button type="button" onclick="list.whitelist()">Whitelist</button>
-	</div>
-	<div style="width: 300px;height: 20px;margin-top: 4px;margin-right: auto;margin-bottom: auto;margin-left: auto;"> 
+	</div><div style="width: 300px;position: fixed;height: 20px;top: 10px;left: 300px;"> 
 		<style>
 			.switch {position: relative; display: inline-block; width: 30px; height: 17px; }
 			.switch input {opacity: 0; width: 0; height: 0; }
@@ -52,15 +56,34 @@ newItem.innerHTML = /* html */`
 			.slider.round:before { border-radius: 50%; }
 		</style>
 		<label class="switch" style=" position: absolute; margin-top: 4px; margin-left: 3px; "> 
-			<input type="checkbox" id="banhistory" checked> 
+			<input type="checkbox" id="countrybanhistory" checked=""> 
 			<span class="slider round"></span>
 		</label>
-		<div style="position: absolute; margin-left: 38px;">No same person twice</div>
+		<div style="position: absolute; margin-left: 38px;">Enable Country Filter</div>
 		
 	</div>
-	<div style="position: absolute;width: 200px;right: 260px;top: 0px;color: #9CF;font-size: 1.1em;">
+	<div style="width: 300px;position: fixed;height: 20px;top: 30px;left: 300px;"> 
+		<style>
+			.switch {position: relative; display: inline-block; width: 30px; height: 17px; }
+			.switch input {opacity: 0; width: 0; height: 0; }
+			.slider { position: absolute; cursor: pointer; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; -webkit-transition: .4s; transition: .4s; }
+			.slider:before {position: absolute; content: ""; height: 13px; width: 13px; left: 2px; bottom: 2px; background-color: white; -webkit-transition: .4s; transition: .4s; }
+			input:checked + .slider { background-color: #2196F3; }
+			input:focus + .slider { box-shadow: 0 0 1px #2196F3; }
+			input:checked + .slider:before { -webkit-transform: translateX(13px); -ms-transform: translateX(13px); transform: translateX(13px); }
+			/* Rounded sliders */
+			.slider.round { border-radius: 17px; }
+			.slider.round:before { border-radius: 50%; }
+		</style>
+		<label class="switch" style=" position: absolute; margin-top: 4px; margin-left: 3px; "> 
+			<input type="checkbox" id="banhistory" checked=""> 
+			<span class="slider round"></span>
+		</label>
+		<div style="position: absolute; margin-left: 38px;">No Same IP</div>
+		
+	</div><div style="position: absolute;width: 200px;right: 260px;top: 0px;color: #9CF;font-size: 1.1em;">
 		<strong id="twicestat">0</strong> Seen more than once<br>
-		<strong id="banstat">0</strong> Banned<br>
+		<strong id="banstat">18</strong> Banned<br>
 		<strong id="skipstat">0</strong> Skipped because ip ban<br>
 	</div>	
 	`;
